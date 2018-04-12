c8773d7f18072c9eb8f2629a4a9fc11604bf9263
Prabu Dhakshinamurthy <prdhaksh@microsoft.com>
2018-04-11 18:42:16 -0700

    Test

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 class test
 {
     public static void main(String[] args) {
-        System.out.println("Hello Visual Studio Code!");
+        System.out.println("Hello Visual Studio Code!!");
     }
 }
