caf8d18c8cf0eac526ec0a49b57603d3572f62b3
Prabu Dhakshinamurthy <prdhaksh@microsoft.com>
2018-04-23 01:00:21 -0700

    HouseRobber

---
+++ HouseRobber.java
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/problems/house-robber/description/
+
+You are a professional robber planning to rob houses along a street.
+Each house has a certain amount of money stashed, the only constraint stopping you from robbing
+each of them is that adjacent houses have security system connected and it will automatically
+contact the police if two adjacent houses were broken into on the same night.
+
+Given a list of non-negative integers representing the amount of money of each house,
+determine the maximum amount of money you can rob tonight without alerting the police.
+*/
+class HouseRobber {
+    public int rob(int[] nums) {
+        int prevMax = 0;
+        int prev = 0;
+        
+        for(int i=0; i<nums.length; i++) {
+            int current = prevMax + nums[i];
+            prevMax = Math.max(prevMax, prev);
+            prev = current;
+        }
+        
+        return Math.max(prevMax, prev);
+    }
+}
